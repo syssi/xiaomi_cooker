4df4e3a3c14c02f8b3c49bbe25db0e485893b6b8
Sebastian Muszynski <basti@linkt.de>
2019-10-12 09:11:43 +0200

    Blackify code

--- custom_components/xiaomi_cooker/__init__.py
+++ custom_components/xiaomi_cooker/__init__.py
@@ -9,7 +9,13 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers import discovery
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_TOKEN, CONF_SCAN_INTERVAL, ATTR_ENTITY_ID, )
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_HOST,
+    CONF_TOKEN,
+    CONF_SCAN_INTERVAL,
+    ATTR_ENTITY_ID,
+)
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.event import track_time_interval
 from homeassistant.helpers.dispatcher import dispatcher_send
@@ -17,23 +23,23 @@ from homeassistant.util.dt import utcnow
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Xiaomi Miio Cooker'
-DOMAIN = 'xiaomi_cooker'
-DATA_KEY = 'xiaomi_cooker_data'
-DATA_TEMPERATURE_HISTORY = 'temperature_history'
-DATA_STATE = 'state'
+DEFAULT_NAME = "Xiaomi Miio Cooker"
+DOMAIN = "xiaomi_cooker"
+DATA_KEY = "xiaomi_cooker_data"
+DATA_TEMPERATURE_HISTORY = "temperature_history"
+DATA_STATE = "state"
 
 SCAN_INTERVAL = timedelta(seconds=30)
 
-CONF_MODEL = 'model'
+CONF_MODEL = "model"
 
-MODEL_PRESSURE1 = 'chunmi.cooker.press1'
-MODEL_PRESSURE2 = 'chunmi.cooker.press2'
-MODEL_NORMAL1 = 'chunmi.cooker.normal1'
-MODEL_NORMAL2 = 'chunmi.cooker.normal2'
-MODEL_NORMAL3 = 'chunmi.cooker.normal3'
-MODEL_NORMAL4 = 'chunmi.cooker.normal4'
-MODEL_NORMAL5 = 'chunmi.cooker.normal5'
+MODEL_PRESSURE1 = "chunmi.cooker.press1"
+MODEL_PRESSURE2 = "chunmi.cooker.press2"
+MODEL_NORMAL1 = "chunmi.cooker.normal1"
+MODEL_NORMAL2 = "chunmi.cooker.normal2"
+MODEL_NORMAL3 = "chunmi.cooker.normal3"
+MODEL_NORMAL4 = "chunmi.cooker.normal4"
+MODEL_NORMAL5 = "chunmi.cooker.normal5"
 
 SUPPORTED_MODELS = [
     MODEL_PRESSURE1,
@@ -45,39 +51,45 @@ SUPPORTED_MODELS = [
     MODEL_NORMAL5,
 ]
 
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_TOKEN): vol.All(cv.string,
-                                          vol.Length(min=32, max=32)),
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_MODEL): vol.In(SUPPORTED_MODELS),
-        vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL):
-            cv.time_period,
-    })
-}, extra=vol.ALLOW_EXTRA)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_HOST): cv.string,
+                vol.Required(CONF_TOKEN): vol.All(
+                    cv.string, vol.Length(min=32, max=32)
+                ),
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                vol.Optional(CONF_MODEL): vol.In(SUPPORTED_MODELS),
+                vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
-ATTR_MODEL = 'model'
-ATTR_PROFILE = 'profile'
+ATTR_MODEL = "model"
+ATTR_PROFILE = "profile"
 
-SUCCESS = ['ok']
+SUCCESS = ["ok"]
 
-SERVICE_SCHEMA = vol.Schema({
-    #    vol.Optional(ATTR_ENTITY_ID): cv.entity_ids,
-})
+SERVICE_SCHEMA = vol.Schema(
+    {
+        #    vol.Optional(ATTR_ENTITY_ID): cv.entity_ids,
+    }
+)
 
-SERVICE_SCHEMA_START = SERVICE_SCHEMA.extend({
-    vol.Required(ATTR_PROFILE): cv.string,
-})
+SERVICE_SCHEMA_START = SERVICE_SCHEMA.extend({vol.Required(ATTR_PROFILE): cv.string})
 
-SERVICE_START = 'start'
-SERVICE_STOP = 'stop'
+SERVICE_START = "start"
+SERVICE_STOP = "stop"
 
 
 # pylint: disable=unused-argument
 def setup(hass, config):
     """Set up the platform from config."""
     from miio import Device, DeviceException
+
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
 
@@ -98,27 +110,32 @@ def setup(hass, config):
             miio_device = Device(host, token)
             device_info = miio_device.info()
             model = device_info.model
-            _LOGGER.info("%s %s %s detected",
-                         model,
-                         device_info.firmware_version,
-                         device_info.hardware_version)
+            _LOGGER.info(
+                "%s %s %s detected",
+                model,
+                device_info.firmware_version,
+                device_info.hardware_version,
+            )
         except DeviceException:
             raise PlatformNotReady
 
     if model in SUPPORTED_MODELS:
         from miio import Cooker
+
         cooker = Cooker(host, token)
 
         hass.data[DOMAIN][host] = cooker
 
-        for component in ['sensor']:
+        for component in ["sensor"]:
             discovery.load_platform(hass, component, DOMAIN, {}, config)
 
     else:
         _LOGGER.error(
-            'Unsupported device found! Please create an issue at '
-            'https://github.com/syssi/xiaomi_cooker/issues '
-            'and provide the following data: %s', model)
+            "Unsupported device found! Please create an issue at "
+            "https://github.com/syssi/xiaomi_cooker/issues "
+            "and provide the following data: %s",
+            model,
+        )
         return False
 
     def update(event_time):
@@ -131,12 +148,14 @@ def setup(hass, config):
             hass.data[DATA_KEY][host][DATA_STATE] = state
 
             if state.mode in [OperationMode.Running, OperationMode.AutoKeepWarm]:
-                hass.data[DATA_KEY][host][DATA_TEMPERATURE_HISTORY] = cooker.get_temperature_history()
+                hass.data[DATA_KEY][host][
+                    DATA_TEMPERATURE_HISTORY
+                ] = cooker.get_temperature_history()
 
-            dispatcher_send(hass, '{}_updated'.format(DOMAIN), host)
+            dispatcher_send(hass, "{}_updated".format(DOMAIN), host)
 
         except DeviceException as ex:
-            dispatcher_send(hass, '{}_unavailable'.format(DOMAIN), host)
+            dispatcher_send(hass, "{}_unavailable".format(DOMAIN), host)
             _LOGGER.error("Got exception while fetching the state: %s", ex)
 
     update(utcnow())
@@ -152,10 +171,10 @@ def setup(hass, config):
         cooker.stop()
 
     hass.services.register(
-        DOMAIN, SERVICE_START, start_service, schema=SERVICE_SCHEMA_START)
+        DOMAIN, SERVICE_START, start_service, schema=SERVICE_SCHEMA_START
+    )
 
-    hass.services.register(
-        DOMAIN, SERVICE_STOP, stop_service, schema=SERVICE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_STOP, stop_service, schema=SERVICE_SCHEMA)
 
     return True
 
@@ -196,6 +215,7 @@ class XiaomiMiioDevice(Entity):
     def device_state_attributes(self):
         """Return the state attributes of the device."""
         return self._state_attrs
+
 
 #    async def _try_command(self, mask_error, func, *args, **kwargs):
 #        """Call a device command handling error messages."""
--- custom_components/xiaomi_cooker/sensor.py
+++ custom_components/xiaomi_cooker/sensor.py
@@ -11,24 +11,24 @@ from homeassistant.util import slugify
 
 _LOGGER = logging.getLogger(__name__)
 
-COOKER_DOMAIN = 'xiaomi_cooker'
-DATA_KEY = 'xiaomi_cooker_data'
-DATA_TEMPERATURE_HISTORY = 'temperature_history'
-DATA_STATE = 'state'
+COOKER_DOMAIN = "xiaomi_cooker"
+DATA_KEY = "xiaomi_cooker_data"
+DATA_TEMPERATURE_HISTORY = "temperature_history"
+DATA_STATE = "state"
 
 SENSOR_TYPES = {
-    'mode': ['Mode', None, 'mode', None],
-    'menu': ['Menu', None, 'menu', None],
-    'temperature': ['Temperature', None, 'temperature', '°C'],
-    'remaining': ['Remaining', None, 'remaining', 'min'],
-    'duration': ['Duration', None, 'duration', 'min'],
-    'favorite': ['Favorite', None, 'favorite', None],
-    'state': ['State', 'stage', 'state', None],
-    'rice_id': ['Rice Id', 'stage', 'rice_id', None],
-    'taste': ['Taste', 'stage', 'taste', None],
-    'taste_phase': ['Taste Phase', 'stage', 'taste_phase', None],
-    'stage_name': ['Stage Name', 'stage', 'name', None],
-    'stage_description': ['Stage Description', 'stage', 'description', None],
+    "mode": ["Mode", None, "mode", None],
+    "menu": ["Menu", None, "menu", None],
+    "temperature": ["Temperature", None, "temperature", "°C"],
+    "remaining": ["Remaining", None, "remaining", "min"],
+    "duration": ["Duration", None, "duration", "min"],
+    "favorite": ["Favorite", None, "favorite", None],
+    "state": ["State", "stage", "state", None],
+    "rice_id": ["Rice Id", "stage", "rice_id", None],
+    "taste": ["Taste", "stage", "taste", None],
+    "taste_phase": ["Taste Phase", "stage", "taste_phase", None],
+    "stage_name": ["Stage Name", "stage", "name", None],
+    "stage_description": ["Stage Description", "stage", "description", None],
 }
 
 
@@ -47,7 +47,6 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class XiaomiCookerSensor(Entity):
-
     def __init__(self, device, host, config):
         """Initialize sensor."""
         self._device = device
@@ -58,12 +57,16 @@ class XiaomiCookerSensor(Entity):
         self._unit_of_measurement = config[3]
         self._state = None
 
-        self.entity_id = ENTITY_ID_FORMAT.format('{}_{}'.format(COOKER_DOMAIN, slugify(self._name)))
+        self.entity_id = ENTITY_ID_FORMAT.format(
+            "{}_{}".format(COOKER_DOMAIN, slugify(self._name))
+        )
 
     @asyncio.coroutine
     def async_added_to_hass(self):
         """Register callbacks."""
-        self.hass.helpers.dispatcher.async_dispatcher_connect('xiaomi_cooker_updated', self.async_update_callback)
+        self.hass.helpers.dispatcher.async_dispatcher_connect(
+            "xiaomi_cooker_updated", self.async_update_callback
+        )
 
     @property
     def name(self):
@@ -79,7 +82,9 @@ class XiaomiCookerSensor(Entity):
             return
 
         state = self.hass.data[DATA_KEY][host].get(DATA_STATE)
-        temperature_history = self.hass.data[DATA_KEY][host].get(DATA_TEMPERATURE_HISTORY)
+        temperature_history = self.hass.data[DATA_KEY][host].get(
+            DATA_TEMPERATURE_HISTORY
+        )
 
         if self._child is not None:
             state = getattr(state, self._child, None)
@@ -92,9 +97,12 @@ class XiaomiCookerSensor(Entity):
             if isinstance(value, Enum):
                 self._state = value.name
             else:
-                if self._attr == 'temperature' and \
-                        state.mode in [OperationMode.Running, OperationMode.AutoKeepWarm] and \
-                        temperature_history:
+                if (
+                    self._attr == "temperature"
+                    and state.mode
+                    in [OperationMode.Running, OperationMode.AutoKeepWarm]
+                    and temperature_history
+                ):
                     self._state = temperature_history.temperatures.pop()
                 else:
                     self._state = value
